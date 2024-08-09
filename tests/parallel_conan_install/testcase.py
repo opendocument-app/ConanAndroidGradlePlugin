fa6ccc1b5c464a67150aba98dd605c53989b9b83
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-09 03:17:08 +0300

    Update parallel conan install test

---
+++ tests/parallel_conan_install/testcase.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import os.path
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+
+def main():
+    conan_p_dir = Path(f"{os.getenv("HOME")}/.conan2/p")
+    if os.path.exists(conan_p_dir):
+        shutil.rmtree(conan_p_dir)
+
+    this_directory = Path(__file__).resolve().parent
+    for arch in ("x86", "x86_64"):
+        if os.path.exists(f"build-{arch}"):
+            shutil.rmtree(f"build-{arch}")
+
+    subprocesses = dict()
+    for arch in ("x86", "x86_64"):
+        subprocesses[arch] = subprocess.Popen(
+            ["conan", "install", "conanfile.txt", "--profile=profile-default.txt", f"--output-folder=build-{arch}"],
+            cwd=this_directory,
+        )
+
+    while len(subprocesses) > 0:
+        subprocesses2 = dict()
+        for p in subprocesses.keys():
+            try:
+                return_code = subprocesses[p].wait(timeout=1)
+                if return_code is None:
+                    subprocesses2[p] = subprocesses[p]  
+                elif return_code != 0:
+                    print("FAILURE!", file=sys.stderr)
+                    for pp in subprocesses.keys():
+                        if pp != p:
+                            subprocesses[pp].kill()
+                            sys.exit(1)
+            except subprocess.TimeoutExpired:
+                pass
+        subprocesses = subprocesses
+
+
+if __name__ == "__main__":
+    main()
